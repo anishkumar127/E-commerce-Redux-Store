eabd26f569150866f354f594e6914a445970813c
Anish Kumar <anishbishnoi127@gmail.com>
2022-12-12 22:24:02 +0530

    persist stroage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
